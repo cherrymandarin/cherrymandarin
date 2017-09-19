55018e30e6ae682ded5531c9360a30a92a581b41
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2017-09-19 11:43:39 +0300

    pohjaa kasaan

---
+++ unity_project/Assets/scripts/JerryManderin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JerryManderin : MonoBehaviour {
+
+    public List<GameObject> love_group;
+    public List<GameObject> hate_group;
+    public List<GameObject> neutral_group;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ unity_project/Assets/scripts/Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+    public GameObject threesame;
+    public GameObject jerrymandarin;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
