395b4da8a9a41472ca4a6907c2c8b888aff44206
Routalanttu <pyry.takkunen@cs.tamk.fi>
2017-09-20 12:18:18 +0300

    - Put "cool" light into Splash - Added AudioManager object to dev_henri

--- unity_project/Assets/scripts/Splash.cs
+++ unity_project/Assets/scripts/Splash.cs
@@ -5,11 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Splash : MonoBehaviour {
 
+	public Transform _dirLight;
+
 	void Awake () {
-		
+		if (null != _dirLight) {
+			_dirLight.eulerAngles = new Vector3 (_dirLight.eulerAngles.x, -100f, _dirLight.eulerAngles.z);
+		}
 	}
 
 	void Update () {
+		if (null != _dirLight && Mathf.Abs(_dirLight.eulerAngles.y) > 1f) {
+			_dirLight.eulerAngles = new Vector3 (_dirLight.eulerAngles.x, _dirLight.eulerAngles.y + Time.deltaTime * 50f, _dirLight.eulerAngles.z);
+		}
+
 		if (Input.GetMouseButtonUp(0)) {
 			SceneManager.LoadScene ("dev_henri");
 		}
