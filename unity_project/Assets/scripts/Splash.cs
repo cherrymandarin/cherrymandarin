9cb6a7051ea230835d1834f1de643f752c48e79c
Routalanttu <pyry.takkunen@cs.tamk.fi>
2017-09-20 11:38:20 +0300

    Quick MouseButtonUp test

---
+++ unity_project/Assets/scripts/Splash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+
+	void Awake () {
+		
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonUp(0)) {
+			SceneManager.LoadScene ("dev_henri");
+		}
+	}
+}
